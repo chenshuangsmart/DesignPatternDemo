697258e3eef780d9a814961dead448f5cc4f95a9
chenshuang <1091277906@qq.com>
2019-04-08 23:28:39 +0800

    迭代器模式例子
    迭代器模式例子

--- DesignPatternDemo/DesignPatternDemo/ViewController.m
+++ DesignPatternDemo/DesignPatternDemo/ViewController.m
@@ -17,9 +17,29 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    /// 迭代器模式
+    [self iterator];
 }
 
-/// 抽象工厂模式
+/// 迭代器模式
+- (void)iterator {
+    //1.数组迭代器
+    NSArray *array = [NSArray arrayWithObjects:@1, @2, @3, @4, @5, nil];
+    // 获取数组的正序迭代器
+    NSEnumerator *enu1 = [array objectEnumerator];
+    // 获取数组的反序迭代器
+    NSEnumerator *enu2 = [array reverseObjectEnumerator];
+    // 遍历数组
+    id obj = nil;
+    // 正序，获取下一个需要遍历的元素
+    while (obj = [enu1 nextObject]) {
+        NSLog(@"%@", obj);
+    }
+    // 反序，获取下一个需要遍历的元素
+    while (obj = [enu2 nextObject]) {
+        NSLog(@"%@", obj);
+    }
+}
 
 
 @end
